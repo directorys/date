f5f79c2f4a8dece05e9ea88e5b4ae2a491c3d5d2
directorys <128901098+directorys@users.noreply.github.com>
2023-05-03 22:51:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+// Элементы:
+const pageElementText = document.querySelector('.page__text');
+const pageElementButton = document.querySelector('.page__button');
+// Даты:
+const toDate = new Date();
+const warDate = new Date(2022, 1, 24);
+const yearDate = new Date(toDate.getFullYear() + 1, 0, 1);
+// Интерйес для дат и переменная со скопированным текстом:
+let stringCoppy = '';
+// Получаем дни:
+const gettingDate = (today, date) => {
+    const dateNumber = Math.abs(Number(today) - Number(date));
+    const newDateNumber = Math.ceil(dateNumber / (1000 * 60 * 60 * 24));
+    return newDateNumber;
+};
+// Получаем даты:
+const date = {
+    war: gettingDate(toDate, warDate),
+    year: gettingDate(toDate, yearDate)
+};
+// Внедряем текст в элемент и переменную:
+if (pageElementText) {
+    stringCoppy = pageElementText.textContent =
+        'День войны: ' + date.war + '\n' +
+            'Дней до НГ: ' + date.year;
+}
+// Скопировать текст по щлчку:
+pageElementButton?.addEventListener('click', () => {
+    navigator.clipboard.writeText(stringCoppy);
+});
